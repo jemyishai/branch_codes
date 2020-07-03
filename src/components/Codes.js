77c2233ee58c2fd968322a3d93eef024acb69618
Jemyishai <jesse.moskowitz@gmail.com>
2020-07-03 15:24:52 -0400

    added input

--- src/components/Codes.js
+++ src/components/Codes.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
 class Codes extends React.Component {
+  constructor (props){
+    super(props)
+
+    this.state = {
+      fullBranchInput : '',
+      codeBranchInput: ''
+    }
+  }
 
   render(){
     const branches = this.props.branches
-    const branchAsProps = Object.getOwnPropertyNames(this.props.branches)
+    const branchAsProps = Object.getOwnPropertyNames(branches)
 
     return (
       <div>
+      <input
+      type="text"
+      focus placeholder="Enter Branch ie..'Bronx Library Center'"
+      className=""
+      onChange={evt => this.setState({ fullBranchInput: evt.target.value })}
+      /> |
+      <input
+      type="text"
+      focus placeholder="Enter Branch Code ie..'BLC'"
+      className=""
+      onChange={evt => this.setState({ codeBranchInput: evt.target.value })}
+      />
          {branchAsProps.map((item)=><li key={item+1}>{item} : {branches[item]}</li>)}
       </div>
     )
