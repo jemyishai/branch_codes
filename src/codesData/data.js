e3211878927208c2137b7185d778d6a5be22ab2a
Jemyishai <jesse.moskowitz@gmail.com>
2020-07-04 10:36:36 -0400

    changed branch data structure

--- src/codesData/data.js
+++ src/codesData/data.js
@@ -3,7 +3,7 @@ import branchCodes from './branchCodes.js';
 let data = [];
 for (const pro in branchCodes) {
   let value = branchCodes[pro]
-	data.push(JSON.parse(`{ "${pro}" : "${value}" }`));
+	data.push(JSON.parse(`{ "branch": "${pro}", "code" : "${value}" }`));
 }
 
 export default data;
--- src/components/Codes.js
+++ src/components/Codes.js
@@ -14,27 +14,25 @@ class Codes extends React.Component {
   render(){
     const branches = this.props.branches
     console.log('branches: ',branches)
-    const branchAsProps = Object.getOwnPropertyNames(branches)
 
     return (
       <div>
       <input
       type="text"
-      focus={true}
+      focus="true"
       placeholder="Enter Branch ie..'Bronx Library Center'"
       className=""
       onChange={evt => this.setState({ fullBranchInput: evt.target.value })}
       /> | | &nbsp;
       <input
       type="text"
-      focus={true}
+      focus="true"
       placeholder="Enter Branch Code ie..'BLC'"
       className=""
       onChange={evt => this.setState({ codeBranchInput: evt.target.value })}
       />
       <ul>
-    { branches.map((object)=> <li key={Object.keys(object)+1}> {Object.keys(object)} : {object[Object.keys(object)]}</li>
-      )
+    { branches.map((object)=> <li key={object.branch+1}> {object.branch} : {object.code}</li>)
     }
          {/* branchAsProps.map((item)=><li key={item+1}>{Object.getOwnPropertyNames(item)} : {branches[item]}</li>) */}
       </ul>
@@ -42,14 +40,13 @@ class Codes extends React.Component {
     )
   }
 
-  filterCodes (code) {
-    const branches = this.props.branches;
+  filterCodes (input) {
 
     const branchInput = new RegExp(this.state.fullBranchInput, 'i');
     const branchCodeInput = new RegExp(this.state.codeBranchInput, 'i');
 
-    return branchInput.test(branches)
-      && branchCodeInput.test(branches)
+    return branchInput.test(input.branch)
+      && branchCodeInput.test(input.code)
 
   }
 
