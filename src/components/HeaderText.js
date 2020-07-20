d7265f90756db527603bf0b25364f520c3ad031e
Jemyishai <jesse.moskowitz@gmail.com>
2020-07-19 23:52:27 -0400

    added one media query & positioning

--- src/components/HeaderText.js
+++ src/components/HeaderText.js
@@ -4,7 +4,7 @@ const HeaderText = () => {
 	return (
 		<div>
 			<p className='directions-padding'>
-				This is a tool for branch librarians to quickly find branch codes. Begin typing and your input will automatically filter the list below.
+				This is a tool for NYPL librarians to quickly find branch codes. Begin typing and your input will automatically filter the list below.
 			</p>
 		</div>
 	);
