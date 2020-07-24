12e46708e0ebad02af69b62414eac3897669a23c
Jemyishai <jesse.moskowitz@gmail.com>
2020-07-24 14:16:08 -0400

    Updates

---
+++ precache-manifest.7729b36043c52de4ff426b92909beae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece70be932113bc736313928496ec228",
+    "url": "/branch_codes/index.html"
+  },
+  {
+    "revision": "576906a43a8f464f1910",
+    "url": "/branch_codes/static/css/main.0cc6e0cf.chunk.css"
+  },
+  {
+    "revision": "c51f955afa5a4129dfcb",
+    "url": "/branch_codes/static/js/2.a1cf4522.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/branch_codes/static/js/2.a1cf4522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576906a43a8f464f1910",
+    "url": "/branch_codes/static/js/main.ea5ab6f4.chunk.js"
+  },
+  {
+    "revision": "99ea3442ad9b344a59b8",
+    "url": "/branch_codes/static/js/runtime-main.4d11d104.js"
+  }
+]);
